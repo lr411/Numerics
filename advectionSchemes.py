9eb46c733a40a9a47793bc522a889a9be8bc484e
Leonardo Ripoli <lr411@imperial.ac.uk>
2017-10-16 12:03:31 +0100

    first work on the new struct: added ftbs function

--- advectionSchemes.py
+++ advectionSchemes.py
@@ -1,11 +1,25 @@
 # Numerical schemes for simulating linear advection for outer code
 # linearAdvect.py 
 
-from __future__ import absolute_import, division, print_function
+#import numpy as np
 
-import numpy as np
-
-def FTCS(phiOld, c, nt):
-    "Linear advection of profile in phiOld using FTCS, Courant number c"
+def FTBS(phiOld, c, nt):
+    "Linear advection of profile in phiOld using FTBS, Courant number c"
     "for nt time-steps"
+    
+    # Calculate nr of space points in our array
+    nx=len(phiOld)
+    
+    # Init array with old data
+    phi=phiOld.copy()
+    
+    # Start iterations 
+    for it in range(nt):
+        # In the following inner loop ix will iterate 1 to nx-1 included
+        for ix in range(1, nx):
+            phi[ix]=phiOld[ix]-c*(phiOld[ix]-phiOld[ix-1])
 
+    phi[0]=phiOld[ix]-c*(phiOld[1]-phiOld[nx-2])
+
+    # Get phiOld ready for the next loop
+    phiOld=phi.copy()
--- linearAdvect.py
+++ linearAdvect.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python
+
+# Numerical analysis for advection scheme
+# The following files are included
+# initialConditions.py : initial condition
+# advectionSchemes.py : functions with advection schemes: FTBS (naive, \
+#      used for comparisons)
+# linearAdvect.py : this file
+
+
