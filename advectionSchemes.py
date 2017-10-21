10ec25b661e8081808ac719a3b691094d6e75987
Leonardo Ripoli <lr411@imperial.ac.uk>
2017-10-21 08:54:14 +0100

    Adding CTCS (work in progress)

--- advectionSchemes.py
+++ advectionSchemes.py
@@ -14,8 +14,7 @@ x=np.linspace(0,1,nx)
 '''
 
 
-def FTBS(phiOld, c, nt):
-    
+def FTBS(phiOld, c, nt):    
     "Linear advection scheme using FTBS, with Courant number c and"
     "                       nt time-steps"
     "inputs are:"
@@ -44,6 +43,35 @@ def FTBS(phiOld, c, nt):
 
     return phi
 
+
+def CTCS(phiOld, c, nt):    
+    "Linear advection scheme using CTCS, with Courant number c and"
+    "                       nt time-steps"
+    "inputs are:"
+    "phiOld: initial condition on phi (the array will then be used to"
+    "        store values from the previous time step)"
+    "c: Courant number"
+    "nt: nr of time steps"
+    
+    # Calculate nr of space points in our array
+    nx=len(phiOld)
+
+    # Init array with old data
+    phi=phiOld.copy()
+    
+    # Start iterations
+    for it in range(nt):
+        # In the following inner loop ix will iterate 1 to nx-1 included
+        for ix in range(1, nx):
+            phi[ix]=phiOld[ix]-c*(phiOld[ix]-phiOld[ix-1])
+
+        phi[0]=phiOld[ix]-c*(phiOld[1]-phiOld[nx-2])
+
+        # Get phiOld ready for the next loop
+        phiOld=phi.copy()
+
+
+    return phi
 
 
 '''
