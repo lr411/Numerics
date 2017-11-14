9f18fd9978b813ed6f9495395af19ad7734cb1ef
Leonardo Ripoli <lr411@imperial.ac.uk>
2017-11-14 18:30:31 +0000

    Commit for report submission

--- linearAdvect.py
+++ linearAdvect.py
@@ -40,8 +40,8 @@ def main(nx, nt, c):
     x=np.linspace(0,1,nx)
 
     #take an initial condition from file initialConditions.py
-    #phi_ic=ic.cosineBasedFctn(x, 0.5)
-    phi_ic=ic.squareWave(x, 0, 0.5)
+    phi_ic=ic.cosineBasedFctn(x, 0.5)
+    #phi_ic=ic.squareWave(x, 0, 0.5)
 
     # in the linear adv eqn exact soln=initial condition shifted by \
     # c*nt*dx, therefore the shift in position in the array is c*nt \
