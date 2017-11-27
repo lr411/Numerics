a61eab6cd89dc70536d31f536ccfb8594c6a2de9
Leonardo Ripoli <lr411@imperial.ac.uk>
2017-11-27 18:17:55 +0000

    adding some useful comments

--- advectionSchemes.py
+++ advectionSchemes.py
@@ -25,21 +25,69 @@ x=np.linspace(0,1,nx)
 # masses=zeros(61)
 
 
-def FTBS(phiOld, c, nt):    
+def LaxWendroff(phiOld, c, nt):    
     """
-    Linear advection scheme using FTBS, with Courant number c and
-                           nt time-steps
+    Linear advection scheme using Lax Wendroff method,
+    with Courant number c and nt time-steps
     inputs are:
-    phiOld: initial condition on phi (to save space the array will then \
+    phiOld (array of floats):
+        initial condition on phi (to save space the array will then \
                    be used to store values from the previous time step)
-    c: Courant number
-    nt: nr of time steps
+    c (float): Courant number
+    nt (int): nr of time steps
+
+    output:
+    phi (array of floats):
+        vector containing the result of the scheme after nt time steps
     """
     
     # Calculate nr of space points in our array
     nx=len(phiOld)
 
-    # Init array with old data
+    # Crerate and init the output array of required size
+    phi=zeros(nx)
+    
+    # Start iterations
+    for it in range(nt):
+        # In the following inner loop ix will iterate 1 to nx-1 included
+        for ix in range(1, nx):
+            phi[ix]=phiOld[ix]-c*(phiOld[ix]-phiOld[ix-1])
+
+        #we apply periodic boundary conditions
+        #phi[0]=phiOld[ix]-c*(phiOld[1]-phiOld[nx-2])
+        phi[0]=phi[nx-1]
+
+        # Get phiOld ready for the next loop
+        phiOld=phi.copy()
+        
+        
+        # check masses for conservation
+        # masses[it]=mean(phi)
+
+
+    return phi
+
+
+def FTBS(phiOld, c, nt):    
+    """
+    Linear advection scheme using FTBS, with Courant number c and
+                           nt time-steps
+    inputs are:
+    phiOld (array of floats):
+        initial condition on phi (to save space the array will then \
+                   be used to store values from the previous time step)
+    c (float): Courant number
+    nt (int): nr of time steps
+
+    output:
+    phi (array of floats):
+        vector containing the result of the scheme after nt time steps
+    """
+    
+    # Calculate nr of space points in our array
+    nx=len(phiOld)
+
+    # Crerate and init array of required size
     phi=zeros(nx)
     
     # Start iterations
@@ -67,17 +115,25 @@ def FTCS(phiOld, c, nt):
     """
     Linear advection scheme using FTCS, with Courant number c and
                            nt time-steps
+    WARNING: this scheme is only stable for c=0, therefore it is only used
+             in this project to calculate the first time step of the
+             CTCS method
     inputs are:
-    phiOld: initial condition on phi (to save space the array will then\
+    phiOld (array of floats):
+        initial condition on phi (to save space the array will then \
                    be used to store values from the previous time step)
-    c: Courant number
-    nt: nr of time steps
+    c (float): Courant number
+    nt (int): nr of time steps
+
+    output:
+    phi (array of floats):
+        vector containing the result of the scheme after nt time steps
     """
     
     # Calculate nr of space points in our array
     nx=len(phiOld)
 
-    # Init array with old data
+    # Crerate and init array of required size
     phi=zeros(nx)
     
     # Start iterations
@@ -107,9 +163,14 @@ def CTCS(phi_ic, c, nt):
     we will need two phi vectors to store data from previous two time steps
     the first time step needed for the method is computed using FTBS
     inputs are:
-    phi_ic: initial condition on phi
-    c: Courant number
-    nt: nr of time steps
+    phi_ic (array of floats):
+        initial condition on phi
+    c (float): Courant number
+    nt (int): nr of time steps
+
+    output:
+    phi_np1 (array of floats):
+        vector containing the result of the scheme after nt time steps
     """
     
     # Calculate nr of space points in our array
@@ -118,10 +179,10 @@ def CTCS(phi_ic, c, nt):
     # Create future time step array and init to zero
     phi_np1=zeros(nx)
 
-    # copy the initial condition into the n-1 array    
+    # copy the initial condition into the n-1 array
     phi_nm1=phi_ic.copy()
 
-    # Calculate array of previous time step doing 1 FTBS step from i.c.
+    # Calculate array of previous time step doing 1 FTCS step from i.c.
     phi_n=FTCS(phi_ic, c, 1)
     
     # Change of name phi_ic becomes the vector where we store time step -2
